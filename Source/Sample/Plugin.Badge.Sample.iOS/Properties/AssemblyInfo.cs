122b3c721a390ff1100752464d3eeb526baf870a
xabre <adri.sece@gmail.com>
2016-12-19 20:52:40 +0100

    Updated version to 1.0.1

--- Source/Sample/Plugin.Badge.Sample.iOS/Properties/AssemblyInfo.cs
+++ Source/Sample/Plugin.Badge.Sample.iOS/Properties/AssemblyInfo.cs
@@ -31,8 +31,8 @@ using Xamarin.Forms;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
 
 //register renderer
 [assembly: ExportRenderer(typeof(TabbedPage), typeof(BadgedTabbedPageRenderer))]
