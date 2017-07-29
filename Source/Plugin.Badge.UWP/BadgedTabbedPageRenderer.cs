8456734f3bbe1439636d772afa2e25dcfdbc6d85
xabre <adri.sece@gmail.com>
2017-07-30 00:55:59 +0200

    Added base UWP project

---
+++ Source/Plugin.Badge.UWP/BadgedTabbedPageRenderer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms.Platform.UWP;
+
+namespace Plugin.Badge.UWP
+{
+    public class BadgedTabbedPageRenderer : TabbedPageRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            var template = Control.ItemTemplate;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+        }
+    }
+}
