c3fdb7cf2edf8285b74a08a13cdc89d85dec5048
Adrian Seceleanu <xabre@users.noreply.github.com>
2017-08-06 19:59:46 +0200

    Added basic mac stuff

---
+++ Source/Plugin.Badge.Mac/BadgedTabbedPageRenderer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using AppKit;
+using Xamarin.Forms.Platform.MacOS;
+
+namespace Plugin.Badge.Mac
+{
+    public class BadgedTabbedPageRenderer : TabbedPageRenderer
+    {
+        public BadgedTabbedPageRenderer()
+        {
+        }
+
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            
+            base.OnElementChanged(e);
+
+            foreach(var item in this.TabViewItems){
+                item.ToolTip = "bla";
+            }
+
+
+
+
+        }
+
+        public override void ViewWillAppear()
+        {
+            base.ViewWillAppear();
+
+
+			//foreach (var item in this.TabViewItems)
+			//{
+			//	item.ToolTip = "blabla";
+   //             item.View.Layer.BackgroundColor = new CoreGraphics.CGColor(0.1f, 0.5f, 0.7f);
+               
+   //             Console.WriteLine(item.View.GetType().Name);
+   //             foreach(var view in item.View.Subviews){
+   //                 Console.WriteLine(view.GetType().Name);
+   //             }
+			//}
+
+			foreach (var view in this.View.Subviews)
+			{
+				Console.WriteLine(view.GetType().Name);
+                if(view is NSSegmentedControl){
+                    var segmentedControl = view as NSSegmentedControl;
+                    segmentedControl.SegmentStyle = NSSegmentStyle.TexturedSquare;
+
+                    foreach (var segment in segmentedControl.Subviews)
+                    {
+                        
+                        var badge = new NSView(new CoreGraphics.CGRect(0, 0, 20, 20));
+                        badge.WantsLayer = true;
+                        badge.Layer.BackgroundColor = new CoreGraphics.CGColor(1f, 0f, 0f);
+                        //badge.AddConstraints(new []{});
+
+                        segment.AddSubview(badge);
+						var constraint = NSLayoutConstraint.Create(badge, NSLayoutAttribute.Right, NSLayoutRelation.Equal);
+                        badge.AddConstraint(constraint);
+
+
+						Console.WriteLine(segment.GetType().Name);
+                    }
+                }
+			}
+
+
+
+        }
+    }
+}
---
+++ Source/Sample/Plugin.Badge.Sample.Mac/AppDelegate.cs
@@ -0,0 +1,37 @@
+﻿using AppKit;
+using Foundation;
+using Xamarin.Forms.Platform.MacOS;
+using Xamarin.Forms;
+
+//register renderer
+[assembly: ExportRenderer(typeof(TabbedPage), typeof(Plugin.Badge.Mac.BadgedTabbedPageRenderer))]
+
+namespace Plugin.Badge.Sample.Mac
+{
+	[Register("AppDelegate")]
+	public class AppDelegate : FormsApplicationDelegate
+	{
+		NSWindow _window;
+		public AppDelegate()
+		{
+			var style = NSWindowStyle.Closable | NSWindowStyle.Resizable | NSWindowStyle.Titled;
+
+			var rect = new CoreGraphics.CGRect(200, 1000, 500, 768);
+			_window = new NSWindow(rect, style, NSBackingStore.Buffered, false);
+			_window.Title = "Xamarin.Forms Badge Plugin on Mac!";
+			_window.TitleVisibility = NSWindowTitleVisibility.Hidden;
+		}
+
+		public override NSWindow MainWindow
+		{
+			get { return _window; }
+		}
+
+		public override void DidFinishLaunching(NSNotification notification)
+		{
+			Forms.Init();
+			LoadApplication(new App());
+			base.DidFinishLaunching(notification);
+		}
+	}
+}
